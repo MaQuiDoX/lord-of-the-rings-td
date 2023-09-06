1297c1170963b762686af02922158927754a5f32
Matías Quesada <matuquesada999@gmail.com>
2023-09-05 22:12:55 -0300

    Pequeñas modificaciones al menu
    - Todavia queda fixearlo, pero es un avance
    - Integracion imprimir cerro en matriz fallido

--- lord-of-the-rings-td/src/Juego/Juego.java
+++ lord-of-the-rings-td/src/Juego/Juego.java
@@ -7,31 +7,34 @@ public class Juego {
         Scanner scanner = new Scanner(System.in);
         int opcion = 0;
 
+        System.out.println(" === DEFIENDE EL CERRO DE LA GLORIA === ");
+        System.out.println("        1 . DIFICULTAD FACIL   ");
+        System.out.println("        2 . DIFICULTAD MEDIA   ");
+        System.out.println("        3 . DIFICULTAD DIFICIL ");
+        System.out.println("        4 . CERRAR ");
+        System.out.println("  ");
         do {
-            System.out.println(" === DEFIENDE EL CERRO DE LA GLORIA === ");
-            System.out.println("        1 . DIFICULTAD FACIL   ");
-            System.out.println("        2 . DIFICULTAD MEDIA   ");
-            System.out.println("        3 . DIFICULTAD DIFICIL ");
-            System.out.println("        4 . CERRAR ");
-            System.out.println("  ");
             System.out.println("Ingrese una opción");
             opcion = scanner.nextInt();
 
             switch (opcion) {
                 case 1:
                     Mapa.printMapa1(args);
+                    break;
                 case 2:
                     //iniciarPartida(2)
+                    break;
                 case 3:
                     //iniciarPartida(3)
+                    break;
                 case 4:
                     System.out.println("Cerrando...");
-                    break;
+                    System.exit(0);
                 default:
                     System.out.println("Opción inválida. Ingrese de nuevo.");
 
             }
-        } while (opcion != 3);
+        } while (opcion != 1 || opcion != 2 || opcion != 3);
     }
 
     //private void iniciarPartida()
