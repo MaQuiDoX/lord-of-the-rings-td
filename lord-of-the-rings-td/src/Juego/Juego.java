2dd3c9f3b15b185a251f614ed22816fec9199dbe
Matías Quesada <matuquesada999@gmail.com>
2023-09-07 00:42:55 -0300

    Interfaz interactiva añadida
    - Añadida distintas opciones al inicializar la partida.
    - QUEDA ARREGLAR, HAY BUGS

--- lord-of-the-rings-td/src/Juego/Juego.java
+++ lord-of-the-rings-td/src/Juego/Juego.java
@@ -29,18 +29,21 @@ public class Juego {
                 System.out.println();
                 Mapa.printMapa1(1);
                 Jugador.mostrarInterfaz();
+                iniciarPartida();
                 break;
             case 2:
                 ClearScreen.cls();
                 System.out.println();
                 Mapa.printMapa1(2);
                 Jugador.mostrarInterfaz();
+                iniciarPartida();
                 break;
             case 3:
                 ClearScreen.cls();
                 System.out.println();
                 Mapa.printMapa1(3);
                 Jugador.mostrarInterfaz();
+                iniciarPartida();
                 break;
             case 4:
                 System.out.println("Cerrando...");
@@ -48,11 +51,130 @@ public class Juego {
         }
     }
 
-    //private void iniciarPartida()
-    //en este segun numero ingresado seria elegir una dificultad
+    public void iniciarPartida(){
+        int auxiliar = 0;
+        int auxiliarTorresDesc = 0;
+        int auxiliarEnemDesc = 0;
+        do {
+            auxiliarTorresDesc = 0;
+            auxiliarEnemDesc = 0;
+            Scanner scanner2 = new Scanner(System.in);
+            int opcion2 = 0;
+            System.out.println();
+            Jugador.mostrarOpciones();
 
+            opcion2 = scanner2.nextInt();
+            while (opcion2 != 1 && opcion2 != 2 && opcion2 != 3 && opcion2 != 4 && opcion2 != 5) {
+                System.out.println("Opción inválida. Ingrese de nuevo.");
+                opcion2 = scanner2.nextInt();
+            }
+            switch (opcion2) {
+                case 1:
+                    Scanner scanner3 = new Scanner(System.in);
+                    int opcion3 = 0;
+                    System.out.println();
+                    Jugador.mostrarOpcionesTorres();
+                    opcion3 = scanner3.nextInt();
 
+                    while (opcion3 != 1 && opcion3 != 2 && opcion3 != 3) {
+                        System.out.println("Opción inválida. Ingrese de nuevo.");
+                        opcion3 = scanner3.nextInt();
+                    }
+                    switch (opcion3) {
+                        case 1:
+                            Scanner scanner4 = new Scanner(System.in);
+                            int opcion4 = 0;
+                            System.out.println();
+                            Jugador.mostrarTienda();
+                            opcion4 = scanner4.nextInt();
 
+                            while (opcion4 != 1 && opcion4 != 2 && opcion4 != 3 && opcion4 != 4 && opcion4 != 5 && opcion4 != 6 && opcion4 != 7) {
+                                System.out.println("Opción inválida. Ingrese de nuevo.");
+                                opcion4 = scanner4.nextInt();
+                            }
+
+                            switch (opcion4) {
+                                case 1: //colocar torre basica
+                                case 2: //colocar torre rango
+                                case 3: //colocar torre area
+                                case 4: //colocar torre ralentizadora
+                                case 5: //colocar torre generadora
+                                case 6: //colocar barricada
+                                case 7:
+                                    break;
+                            }
+                        case 2:
+                            //mejora torre
+                        case 3:
+                            break;
+                    }
+                case 2:
+                    do {
+                        Scanner scanner5 = new Scanner(System.in);
+                        int opcion5 = 0;
+                        System.out.println();
+                        Jugador.descripcionTorres();
+                        opcion5 = scanner5.nextInt();
+
+                        while (opcion5 != 1 && opcion5 != 2 && opcion5 != 3 && opcion5 != 4 && opcion5 != 5 && opcion5 != 6 && opcion5 != 7) {
+                            System.out.println("Opción inválida. Ingrese de nuevo.");
+                            opcion5 = scanner5.nextInt();
+                        }
+
+                        switch(opcion5){
+                            case 1: //lore y caracteristicas torre basica
+                                break;
+                            case 2: //lore y caracteristicas torre rango
+                                break;
+                            case 3: //lore y caracteristicas torre area
+                                break;
+                            case 4: //lore y caracteristicas torre ralentizadora
+                                break;
+                            case 5: //lore y caracteristicas torre generadora
+                                break;
+                            case 6: //lore y caracteristicas barricada
+                                break;
+                            case 7:
+                                auxiliarTorresDesc++;
+                        }
+                    } while (auxiliarTorresDesc == 1);
+                case 3:
+                    do {
+                        Scanner scanner6 = new Scanner(System.in);
+                        int opcion6 = 0;
+                        System.out.println();
+                        Jugador.descripcionEnemigos();
+                        opcion6 = scanner6.nextInt();
+
+                        while (opcion6 != 1 && opcion6 != 2 && opcion6 != 3 && opcion6 != 4 && opcion6 != 5 && opcion6 != 6) {
+                            System.out.println("Opción inválida. Ingrese de nuevo.");
+                            opcion6 = scanner6.nextInt();
+                        }
+
+                        switch(opcion6){
+                            case 1: //lore y caracteristicas humano
+                                break;
+                            case 2: //lore y caracteristicas elfo
+                                break;
+                            case 3: //lore y caracteristicas enano
+                                break;
+                            case 4: //lore y caracteristicas hobbit
+                                break;
+                            case 5: //lore y caracteristicas ent
+                                break;
+                            case 6:
+                                auxiliarEnemDesc++;
+                        }
+                    } while (auxiliarEnemDesc == 1);
+                case 4:
+                    // asociado a mapa.array de enemigos
+                case 5:
+                    auxiliar++;
+                default:
+                    break;
+            }
+        } while (auxiliar == 1);
+    }
 }
 
 
--- lord-of-the-rings-td/src/Jugador/Jugador.java
+++ lord-of-the-rings-td/src/Jugador/Jugador.java
@@ -16,7 +16,51 @@ public class Jugador {
         System.out.println("        G: Torre Generadora -- BB: Barricada ");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
-    public static void mostrarTienda(){
 
+    public static void mostrarOpciones() {
+        System.out.println("¿Qué desea hacer?:");
+        System.out.println("1. Fortalecer defensas");
+        System.out.println("2. Descripción de Torres");
+        System.out.println("3. Descripción de Enemigos");
+        System.out.println("4. Conocer siguiente oleada");
+        System.out.println("5. Comenzar oleada");
+    }
+
+    public static void mostrarOpcionesTorres() {
+        System.out.println("¿Qué desea hacer?:");
+        System.out.println("1. Colocar una Torre");
+        System.out.println("2. Mejorar una Torre");
+        System.out.println("3. Volver para atrás");
+    }
+
+    public static void mostrarTienda(){
+        System.out.println("1. Colocar Torre Básica (100 coste de magia)");
+        System.out.println("2. Colocar Torre de Rango (150 coste de magia)");
+        System.out.println("3. Colocar Torre de Área (300 coste de magia)");
+        System.out.println("4. Colocar Torre Ralentizadora (400 coste de magia)");
+        System.out.println("5. Colocar Torre Generadora (200 coste de magia)");
+        System.out.println("6. Colocar Barricada (500 coste de magia)");
+        System.out.println("7. Volver para atrás");
+    }
+
+    public static void descripcionTorres() {
+        System.out.println("¿Acerca de que Torre quieres saber más?");
+        System.out.println("1. Torre Básica");
+        System.out.println("2. Torre de Rango");
+        System.out.println("3. Torre de Área");
+        System.out.println("4. Torre Ralentizadora");
+        System.out.println("5. Torre Generadora");
+        System.out.println("6. Barricada");
+        System.out.println("7. Volver atrás");
+    }
+
+    public static void descripcionEnemigos() {
+        System.out.println("¿Acerca de que Enemigo quieres saber más?");
+        System.out.println("1. Humano");
+        System.out.println("2. Elfo");
+        System.out.println("3. Enano");
+        System.out.println("4. Hobbit");
+        System.out.println("5. Ent");
+        System.out.println("6. Volver atrás");
     }
 }
