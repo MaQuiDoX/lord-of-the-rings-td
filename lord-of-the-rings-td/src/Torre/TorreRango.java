53ba99cdb47d1ea3e207cbc486e1e65be9afc091
EzeMarts <emartinsleiva@gmail.com>
2023-09-18 12:29:44 -0300

    Squashed commit of the following:
    commit 3f1a8df02717830d033396522c93e5f85be869b1
    Author: Ignacio Coppede <nachocoppede@gmail.com>
    Date:   Mon Sep 18 11:50:31 2023 -0300
    
        daño bonus cuando un enemigo es debil a la torre.
    
    commit 56454865765454f20c32af90e22ed97c11acabd6
    Author: Ignacio Coppede <nachocoppede@gmail.com>
    Date:   Mon Sep 18 11:49:46 2023 -0300
    
        daño bonus cuando un enemigo es debil a la torre.
    
    commit da55bdea0555091792bf1b01d7810810bd6fea32
    Merge: a614f9d 7eaa26a
    Author: Ignacio Coppede <nachocoppede@gmail.com>
    Date:   Mon Sep 18 11:49:12 2023 -0300
    
        Merge branch 'main' of https://github.com/MaQuiDoX/lord-of-the-rings-td
    
    commit a614f9d3ee9dca1afc524629fb09af27b4401ed0
    Author: Ignacio Coppede <nachocoppede@gmail.com>
    Date:   Mon Sep 18 11:47:43 2023 -0300
    
        daño bonus cuando un enemigo es debil a la torre.
    
    commit 7eaa26a93a9affa9cd3db99cc7f11b7275fa17b2
    Merge: d170990 1552bc1
    Author: EzeMarts <111715400+EzeMarts@users.noreply.github.com>
    Date:   Mon Sep 18 11:34:58 2023 -0300
    
        Merge pull request #12 from MaQuiDoX/eze1
    
        Resubir Cambios de Enemigos y Celdas (con extras)

--- lord-of-the-rings-td/src/Torre/TorreRango.java
+++ lord-of-the-rings-td/src/Torre/TorreRango.java
@@ -4,7 +4,8 @@ import Celda.CeldaTerreno;
 
 public class TorreRango extends TorreActiva{
 
+
     public TorreRango(CeldaTerreno celdaAsociada){
-        super(20, 2, 4, "rango", 150, 300, celdaAsociada);
+        super(20, 2, 4, "r", 150, 300, celdaAsociada);
     }
 }
