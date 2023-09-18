56683ec4564a3f5e933b14ab19bd431d387b6b30
EzeMarts <emartinsleiva@gmail.com>
2023-09-18 19:59:05 -0300

    Cambios en Enemigo y enemigos
    + Cambios varios en Enemigo:
    - Añadido waitingTick() y actionTick() provisionales
    - Arreglado moverEnemigo()
    + Arreglados constructores de todos los enemigos

--- lord-of-the-rings-td/src/Enemigo/Enano.java
+++ lord-of-the-rings-td/src/Enemigo/Enano.java
@@ -1,7 +1,14 @@
 package Enemigo;
-// añadir commit
+
+import Celda.CeldaCamino;
+
+/**
+ * Una clase que genera un enemigo enano del tipo 'pesado'.
+ * @version 1.0 18/9/23
+ * @author Ezequiel L. Martins
+ */
 public class Enano extends Enemigo{
-    public Enano(){
-        super(320,3,5,12,"Normal","Array");
+    public Enano(CeldaCamino celdaCamino){
+        super(320,3,5,12,"enano",false, celdaCamino);
     }
 }
